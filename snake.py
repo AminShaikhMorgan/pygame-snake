c2917dd0c41252ed09a9761d010aa1a74e57f993
Rushaad <34074634+Rushaad@users.noreply.github.com>
2017-12-05 16:16:41 -0500

    Update snake.py
    Changed directions of snake.

--- snake.py
+++ snake.py
@@ -45,12 +45,15 @@ def get_direction(previous_direction, event_key):
     event_key - the event that the user pressed; one of https://www.pygame.org/docs/ref/key.html
     If event_key does not correspond with any of the arrows keys, return previous_direction.
     """
-    if event_key == pygame.K_LEFT:
+   if event_key == pygame.K_LEFT:
         return DIRECTION_LEFT
     elif event_key == pygame.K_UP:
         return DIRECTION_UP
+    elif event_key == pygame.K_RIGHT:
+        return DIRECTION_RIGHT
+    elif event_key == pygame.K_DOWN:
+        return DIRECTION_DOWN
     return previous_direction
-
 def create_food_position():
     """Returns a random 2-tuple in the grid where the food should be located.
     The first element is the x position. Must be an int between 0 and GRID_WIDTH - 1, inclusively.
