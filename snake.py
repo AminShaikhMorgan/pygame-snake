72947629ab20f896ba13aae83a3527ec3338bab3
Rushaad98 <34074634+Rushaad98@users.noreply.github.com>
2017-12-07 13:25:29 -0500

    Update snake.py
    Adding food for the snake

--- snake.py
+++ snake.py
@@ -59,7 +59,11 @@ def create_food_position():
     The first element is the x position. Must be an int between 0 and GRID_WIDTH - 1, inclusively.
     The second element is the y position. Must be an int between 0 and GRID_HEIGHT - 1, inclusively.
     """
-    return
+    create_food_position = (x_position, y_position)
+    y = random.randint(0,GRID_HEIGHT -1)
+    x = random.randint(0,GRID_WIDTH -1)
+    return (x,y)
+    
 
 def snake_ate_food(snake, food):
     """Returns whether food was eaten by the snake.
@@ -67,6 +71,8 @@ def snake_ate_food(snake, food):
     food - 2-tuple representing the position in the grid of the food
     This function should return True if the head of the snake is in the same position as food.
     """
+     if snake[0] == food:
+        return True
     return False
 
 def snake_ran_out_of_bounds(snake):
